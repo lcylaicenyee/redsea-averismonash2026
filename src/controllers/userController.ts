5ec8676d04e74daf0fd870205755becb935fae54
Lai Cen Yee <cenyee.lai@gmail.com>
2026-09-20 20:09:18 +0800

    Fixed login system in back-end side

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -126,12 +126,14 @@ export const loginUser = async (req: Request, res: Response): Promise<Response> 
 
     return res.status(200).json({
       success: true,
-      token,
-      user: {
-        id: user._id,
-        name: user.name,
-        email: user.email,
-        role: user.role
+      data: {
+        token,
+        user: {
+          id: user._id,
+          name: user.name,
+          email: user.email,
+          role: user.role
+        }
       }
     });
   } catch (error) {
